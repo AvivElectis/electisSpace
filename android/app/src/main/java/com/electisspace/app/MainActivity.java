1e18cdf79a94e8047556d88c3c1a4006a2b62291
AvivElectis <aviv@electis.co.il>
2026-03-25 12:28:43 +0200

    fix: remove deprecated setDatabaseEnabled call (always enabled since API 28)

--- android/app/src/main/java/com/electisspace/app/MainActivity.java
+++ android/app/src/main/java/com/electisspace/app/MainActivity.java
@@ -13,14 +13,11 @@ public class MainActivity extends BridgeActivity {
 
         WebView webView = getBridge().getWebView();
         if (webView != null) {
-            // Ensure WebView has a white background (prevents black screen on emulator)
             webView.setBackgroundColor(Color.WHITE);
 
-            // WebView performance settings
             WebSettings settings = webView.getSettings();
             settings.setCacheMode(WebSettings.LOAD_DEFAULT);
             settings.setDomStorageEnabled(true);
-            settings.setDatabaseEnabled(true);
         }
     }
 }
